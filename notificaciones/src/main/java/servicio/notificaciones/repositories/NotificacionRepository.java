72d04fdda34bfbd072b2ef0710c11962b492a2b1
LuigircDev <luigirc.dev@proton.me>
2024-12-04 18:12:03 -0300

    delete all

--- notificaciones/src/main/java/servicio/notificaciones/repositories/NotificacionRepository.java
+++
@@ -1,9 +0,0 @@
-package servicio.notificaciones.repositories;
-
-
-import servicio.notificaciones.models.Notificacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificacionRepository extends JpaRepository<Notificacion, Integer> {
-}
-
